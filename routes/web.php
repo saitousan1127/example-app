a88ebe88cc484f916dd5ff30de1654834f7878c3
saitousan1127 <saitoumasato1127@gmail.com>
2023-07-03 18:43:18 +0900

    ddd

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,
 
 
 // Tweet
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
+->name('tweet.index');
 Route::middleware('auth')->group(function () {
     Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
     ->name('tweet.create');
@@ -34,8 +36,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)
     ->name('tweet.delete');
 });
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
-->name('tweet.index');
+
 
 
 Route::get('/dashboard', function () {
